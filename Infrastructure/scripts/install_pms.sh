f4d96150972266f5363c1ae07bbb099299c17747
Paul Massé <paul.masse@reseau.eseo.fr>
2023-01-06 11:38:08 +0100

    Script upload branch dev de git

--- Infrastructure/scripts/install_pms.sh
+++ Infrastructure/scripts/install_pms.sh
@@ -9,34 +9,10 @@ echo "START - Configuration Website  - "$IP
 cd /var/www/html
 sleep 3
 
-sudo rm index.html
+sudo git clone --branch Dev https://github.com/Maxime-rja/Pimp_My_Snack_V3.git
 sleep 3
 
-echo "dl de test.html"
-#BURGER HTML
-wget 'https://raw.githubusercontent.com/Maxime-rja/Pimp_My_Snack/Dev/Dev/Template/burger.html?token=GHSAT0AAAAAAB5DJF2OIJCM6DM4LSKTHH4QY5W7KDA'
-sudo cp 'burger.html?token=GHSAT0AAAAAAB5DJF2OIJCM6DM4LSKTHH4QY5W7KDA' burger.html
-sudo rm 'burger.html?token=GHSAT0AAAAAAB5DJF2OIJCM6DM4LSKTHH4QY5W7KDA'
-
-
-
-#CONTACT
-wget 'https://raw.githubusercontent.com/Maxime-rja/Pimp_My_Snack/Dev/Dev/Template/contact.html?token=GHSAT0AAAAAAB5DJF2PUWTDZ4M5HFGXYD5AY5W7CEA'
-sudo cp 'contact.html?token=GHSAT0AAAAAAB5DJF2PUWTDZ4M5HFGXYD5AY5W7CEA' contact.html
-sudo rm 'contact.html?token=GHSAT0AAAAAAB5DJF2PUWTDZ4M5HFGXYD5AY5W7CEA'
-
-#HOME PAGE
-wget 'https://raw.githubusercontent.com/Maxime-rja/Pimp_My_Snack/Dev/Dev/Template/index.html?token=GHSAT0AAAAAAB5DJF2PB2EMO4WXRGL5VHNYY5W7C5Q'
-sudo cp 'index.html?token=GHSAT0AAAAAAB5DJF2PB2EMO4WXRGL5VHNYY5W7C5Q' index.html
-sudo rm 'index.html?token=GHSAT0AAAAAAB5DJF2PB2EMO4WXRGL5VHNYY5W7C5Q'
-
-#BURGER CSS
-mkdir css
-cd css/
-wget 'https://raw.githubusercontent.com/Maxime-rja/Pimp_My_Snack/Dev/Dev/Template/css/style.css?token=GHSAT0AAAAAAB5DJF2ODSREFF2IXGUZCZ6GY5W7ONA'
-sudo cp 'style.css?token=GHSAT0AAAAAAB5DJF2ODSREFF2IXGUZCZ6GY5W7ONA' style.css
-sudo rm 'style.css?token=GHSAT0AAAAAAB5DJF2ODSREFF2IXGUZCZ6GY5W7ONA'
-
+sudo cp -r /var/www/html/Pimp_My_Snack_V3/Template/* /var/www/html/
 
 
 echo "Restarting Apache..."
