454ef2af698618faf785e6ced618b5bfecc75368
Paul Massé <paul.masse@reseau.eseo.fr>
2023-01-17 11:19:30 +0100

    Optimisation du script d'import du git

--- Infrastructure/scripts/install_pms.sh
+++ Infrastructure/scripts/install_pms.sh
@@ -2,16 +2,34 @@
 
 IP=$(hostname -I | awk '{print $2}')
 APT_OPT="-o Dpkg::Progress-Fancy="0" -q -y"
-LOG_FILE="/vagrant/logs/install_pms.log"
-DEBIAN_FRONTEND="noninteractive"
 
+# ADD INSTALL DE GIT + LOG 
+echo "=> [1]: Updating and installing Git..."
+apt-get update $APT_OPT \
+sleep 3
+sudo apt-get -y install git
+
+
+
+# Upgrade le script pour fichiers existants
 echo "START - Configuration Website  - "$IP
 cd /var/www/html
-sleep 3
+#Deleting old files 
+files=`ls -1 *.html 2>/dev/null` 
+if [ -n "$files" ]; then
+  rm -f $files
+  sleep 3
+  sudo rm -r css/ img/ js/ mail/ lib/ mail/ Pimp_My_Snack_V3/
+  echo "Fichiers .html supprimés avec succès"
+else
+  echo "Aucun fichier .html trouvé dans le dossier courant"
+fi
 
+#Cloning the git repertory 
 sudo git clone --branch Dev https://github.com/Maxime-rja/Pimp_My_Snack_V3.git
 sleep 3
 
+#Copying the files to the right path 
 sudo cp -r /var/www/html/Pimp_My_Snack_V3/Template/* /var/www/html/
 
 
