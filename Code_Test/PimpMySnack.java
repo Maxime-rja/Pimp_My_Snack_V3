74631f5a3c0e9411a4d8fe51cf31650180ada849
ClemREV <clement.revaud@reseau.eseo.fr>
2023-01-17 09:07:17 +0100

    Version_0_interface_admin

--- Code_Test/PimpMySnack.java
+++ Code_Test/PimpMySnack.java
@@ -1,5 +1,7 @@
 import java.awt.FlowLayout;
-
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,7 +16,7 @@ public class PimpMySnack extends JFrame
     }
 
     private void build(){
-		setTitle("Ma première fenêtre"); //On donne un titre à l'application
+		setTitle("Pimp My Snack"); //On donne un titre à l'application
 		setSize(1000,500); //On donne une taille à notre fenêtre
 		setLocationRelativeTo(null); //On centre la fenêtre sur l'écran
 		setResizable(false); //On interdit la redimensionnement de la fenêtre
@@ -24,11 +26,90 @@ public class PimpMySnack extends JFrame
 
     private JPanel buildContentPane(){
         JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-     
-        JLabel label = new JLabel("Bienvenue dans ma modeste application");
-     
-        panel.add(label);
+
+        JLabel label_pain = new JLabel("Pain");
+        JButton btn_add_pain = new JButton("+");
+        JButton btn_rm_pain = new JButton("-");
+
+        JLabel label_tomate = new JLabel("Tomates");
+        JButton btn_add_tomate = new JButton("+");
+        JButton btn_rm_tomate = new JButton("-");
+
+        JLabel label_steak = new JLabel("Steak");
+        JButton btn_add_steak = new JButton("+");
+        JButton btn_rm_steak = new JButton("-");
+
+
+        JLabel label_oignon = new JLabel("Oignon");
+        JButton btn_add_oignon = new JButton("+");
+        JButton btn_rm_oignon = new JButton("-");
+
+        JLabel label_champignon = new JLabel("Champignon");
+        JButton btn_add_champignon = new JButton("+");
+        JButton btn_rm_champignon = new JButton("-");
+
+        JLabel label_fromage = new JLabel("Fromage");
+        JButton btn_add_fromage = new JButton("+");
+        JButton btn_rm_fromage = new JButton("-");
+
+        JLabel label_cornichon = new JLabel("Cornichon");
+        JButton btn_add_cornichon = new JButton("+");
+        JButton btn_rm_cornichon = new JButton("-");
+
+        JLabel label_salade = new JLabel("Salade");
+        JButton btn_add_salade = new JButton("+");
+        JButton btn_rm_salade = new JButton("-");
+
+        JLabel label_poulet = new JLabel("Poulet");
+        JButton btn_add_poulet = new JButton("+");
+        JButton btn_rm_poulet = new JButton("-");
+
+        JLabel label_bacon = new JLabel("Bacon");
+        JButton btn_add_bacon = new JButton("+");
+        JButton btn_rm_bacon = new JButton("-");
+
+        panel.add(label_pain);
+        panel.add(btn_add_pain);
+        panel.add(btn_rm_pain);
+
+        panel.add(label_tomate);
+        panel.add(btn_add_tomate);
+        panel.add(btn_rm_tomate);
+
+        panel.add(label_steak);
+        panel.add(btn_add_steak);
+        panel.add(btn_rm_steak);
+
+        panel.add(label_oignon);
+        panel.add(btn_add_oignon);
+        panel.add(btn_rm_oignon);
+
+        panel.add(label_champignon);
+        panel.add(btn_add_champignon);
+        panel.add(btn_rm_champignon);
+
+        panel.add(label_fromage);
+        panel.add(btn_add_fromage);
+        panel.add(btn_rm_fromage);
+
+        panel.add(label_cornichon);
+        panel.add(btn_add_cornichon);
+        panel.add(btn_rm_cornichon);
+
+        panel.add(label_salade);
+        panel.add(btn_add_salade);
+        panel.add(btn_rm_salade);
+
+        panel.add(label_poulet);
+        panel.add(btn_add_poulet);
+        panel.add(btn_rm_poulet);
+
+        panel.add(label_bacon);
+        panel.add(btn_add_bacon);
+        panel.add(btn_rm_bacon);
+
+
+        panel.setLayout(new GridLayout(10,3));
      
         return panel;
     }
