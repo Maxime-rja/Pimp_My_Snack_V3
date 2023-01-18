1f7110e6578c95aa76cc5a523dfecf138505cd9a
Maxime.rja <maxime.rouja@reseau.eseo.fr>
2023-01-18 11:14:17 +0100

    3.2 et 4.1 fonctionnels

--- Infrastructure/backup/scripts/cron_bdd.sh
+++ Infrastructure/backup/scripts/cron_bdd.sh
@@ -10,8 +10,8 @@ grep -qF "$guard_comment" mycron && exit 0
 echo "$guard_comment" >>mycron
 
 #echo "*/1 * * * * echo "Test du crontab" >>mycron
-echo "*/1 * * * * bash /usr/bin/backup/backup.sh" >>mycron #schedule the delete script
-echo "*/1 * * * * sudo mysqldump --databases phpmyadmin > /vagrant/backup/dump/sauvegarde-$(date +%Y%m%d%H%M%S).sql" >>mycron
+echo "*/30 * * * * bash /usr/bin/backup/backup.sh" >>mycron #schedule the delete script
+echo "*/30 * * * * sudo mysqldump --databases phpmyadmin > /vagrant/backup/dump/latest-save.sql" >>mycron
 
 # install new cron file
 crontab mycron
