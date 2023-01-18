51d25e72a569acf802f07a8eebab26e41bdc8273
Maxime.rja <maxime.rouja@reseau.eseo.fr>
2023-01-18 09:05:23 +0100

    maj des fichiers pour changement de branche

--- Infrastructure/backup/scripts/cron_bdd.sh
+++ Infrastructure/backup/scripts/cron_bdd.sh
@@ -8,7 +8,7 @@ grep -qF "$guard_comment" mycron && exit 0
 
 # echo new cron into cron file
 echo "$guard_comment" >>mycron
-echo "*/30 * * * * bash /usr/bin/backup/backup.sh" >>mycron #schedule the delete script
+echo "*/1 * * * * bash /usr/bin/backup/backup.sh" >>mycron #schedule the delete script
 
 # install new cron file
 crontab mycron
