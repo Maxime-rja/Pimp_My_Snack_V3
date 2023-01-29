79992980a999161be219827d10d08f94c14aa494
Maxime.rja <111341774+Maxime-rja@users.noreply.github.com>
2023-01-29 18:09:20 +0100

    Revert "Merge branch 'main' into Main-Infra-Dev"
    This reverts commit 8b9ccde2bc9e6e65c07abfe787914ba54cbff397, reversing
    changes made to 9408c6a845958ccd01484f54ef1d272ebcbcfda1.

--- Infrastructure/files/create_tables.sql
+++ Infrastructure/files/create_tables.sql
@@ -529,4 +529,3 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2023-01-27 15:00:03
-
--- Infrastructure/ssl/scripts/ssl.sh
+++ Infrastructure/ssl/scripts/ssl.sh
@@ -4,9 +4,7 @@ sudo apt install -y git
 sudo apt install -y curl git
 sudo apt install dos2unix
 
-sudo git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.1
-
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.1
 
 dos2unix .bashrc
 cp /vagrant/ssl/.bashrc /root/
-
